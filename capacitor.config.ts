1aafba28fc4551a6096fa048df0c5cb60b4aeb56
Sohel Rana <sohel0130844@gmail.com>
2025-11-01 09:47:14 -0600

    v fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -27,7 +27,6 @@ const config: CapacitorConfig = {
       backgroundColor: '#1e1b4b',
     },
     Keyboard: {
-      resize: 'body',
       style: 'dark',
       resizeOnFullScreen: true,
     },
@@ -38,25 +37,12 @@ const config: CapacitorConfig = {
       keystorePath: undefined,
       keystoreAlias: undefined,
     },
-    permissions: [
-      'RECORD_AUDIO',
-      'MODIFY_AUDIO_SETTINGS',
-      'INTERNET',
-      'ACCESS_NETWORK_STATE',
-      'ACCESS_WIFI_STATE',
-      'VIBRATE',
-      'WAKE_LOCK',
-    ],
   },
   ios: {
     scheme: 'LinguaAI',
     allowsLinkPreview: false,
     scrollEnabled: true,
     contentInset: 'automatic',
-    permissions: [
-      'Microphone',
-      'SpeechRecognition',
-    ],
   },
 };
 
